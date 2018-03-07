ee6e76341becca46cb9ba6699ad056bbe6f7d0c6
Lorenz Leutgeb <lorenz.leutgeb@stud-inf.unibz.it>
2018-03-07 18:54:06 +0100

    Oops,... add changes to data.py

--- Labs/2018-03-07/data.py
+++ Labs/2018-03-07/data.py
@@ -2,7 +2,7 @@
 
 from subprocess import PIPE, STDOUT, run
 from tempfile   import mkstemp
-from random     import random
+from random     import choice, sample
 
 import pandas as pd
 import numpy as np
@@ -16,18 +16,20 @@ k = 3
 N = [50, 100, 200]
 
 # Number of experiments to run in order to determine p.
-m = 100.0
+m = 50.0
 
-R = (1.0, 6.0, 0.01)
+R = (2.0, 6.0, 0.05)
 
 def randomize(n, k, l):
-    return [[1 + int(n * random()) * (-1) * round(random()) for j in range(k)] for i in range(l)]
+    return [[p * -1 if choice([True, False]) else p for p in sample(range(1, n+1), k)] for j in range(l)]
 
 def decode(result):
-    if result.endswith(b'UNSATISFIABLE\n'):
-        return False
-    elif result.endswith(b'SATISFIABLE\n'):
-        return True
+    result = result.strip().split('\n')
+    time = float(result[-3][len('CPU time              : '):-len(' s')])
+    if result[-1] == 'UNSATISFIABLE':
+        return (False, time)
+    elif result[-1] == 'SATISFIABLE':
+        return (True, time)
     else:
         print('Neither satisfiable nor satisfiable!')
         exit(1)
@@ -45,27 +47,30 @@ def solve(n, k, l):
 
     # Write CNF to the input file in DIMACS format.
     with open(ifd, 'w') as f: f.write(dimacs)
-    proc = run(['minisat', ifname], stderr=STDOUT, stdout=PIPE)
-    return decode(proc.stdout)
+    proc = run(['minisat', '-verb=2', ifname], stderr=STDOUT, stdout=PIPE)
+    return decode(proc.stdout.decode('utf-8'))
 
 def p(n, k, l):
     x = 0.0
+    totaltime = 0.0
     for i in range(int(m)):
-        if solve(n, k, l):
+        (satisfiability, time) = solve(n, k, l)
+        if satisfiability:
             x += 1.0
+        totaltime += time
 
-    return x / m
+    return (x / m, totaltime / m)
 
 def main():
-    with open('satisfiability.dat', 'w+') as f:
+    with open('data.dat', 'w+') as f:
         f.write('# r {}\n'.format(N))
 
         r = R[0]
         while r <= R[1]:
             # r = l / n
             # l = r * n
-            f.write('{:4.3f}\t'.format(r) + '\t'.join(['{:4.3f}'.format(p(n, k, int(r * n))) for n in N]) + '\n')
-            r += R[2]
+            f.write('{:4.3f}\t'.format(r) + '\t'.join(['{:4.3f} {:4.3f}'.format(*p(n, k, int(r * n))) for n in N]) + '\n')
+            r += R[2] + 0.07 * (r - R[0] - ((R[1] - R[0]) / 2.0)) ** 2.0
 
 if __name__ == '__main__':
     main()
