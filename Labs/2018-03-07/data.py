a0e8cc01bdbc46be5dc34f2ca6da47b4ada81e04
Lorenz Leutgeb <lorenz.leutgeb@stud-inf.unibz.it>
2018-03-08 09:36:35 +0100

    Don't run out of space on /tmp

--- Labs/2018-03-07/data.py
+++ Labs/2018-03-07/data.py
@@ -1,5 +1,6 @@
 # Team White
 
+from os         import remove
 from subprocess import PIPE, STDOUT, run
 from tempfile   import mkstemp
 from random     import choice, sample
@@ -12,65 +13,79 @@ import matplotlib.pyplot as plt
 # k ... Number of Literals per Clause
 # l ... Number of Clauses
 
-k = 3
-N = [50, 100, 200]
+# List to choose k from.
+K = [2, 3, 5]
+
+# List to choose n from.
+N = [20, 50, 100, 200, 500]
 
 # Number of experiments to run in order to determine p.
-m = 50.0
+m = 200.0
 
-R = (2.0, 6.0, 0.05)
+# Range parameters for r: (min, max, delta)
+R = (2.0, 6.0, 0.01)
 
 def randomize(n, k, l):
     return [[p * -1 if choice([True, False]) else p for p in sample(range(1, n+1), k)] for j in range(l)]
 
+def average(n, k, l):
+    # r = l / n
+    # l = r * n
+    return tuple([sum(x) / m for x in zip(*[solve(n, k, l) for i in range(int(m))])])
+
+def dots(r):
+    return '{:4.3f}'.format(r) + '\t' + '\t'.join([
+        '\t'.join(
+            [
+                '{:4.3f} {:7.3f} {:7.3f}'.format(*average(n, k, int(r * n))) for n in N
+            ]
+        ) for k in K
+    ]) + '\n'
+
 def decode(result):
     result = result.strip().split('\n')
-    time = float(result[-3][len('CPU time              : '):-len(' s')])
-    if result[-1] == 'UNSATISFIABLE':
-        return (False, time)
-    elif result[-1] == 'SATISFIABLE':
-        return (True, time)
-    else:
-        print('Neither satisfiable nor satisfiable!')
-        exit(1)
+
+    # MiniSat output is in a two-column style, this is the offset
+    # of the second column which holds the data.
+    off = 24
+
+    # Extract number of decisions made by MiniSat.
+    decisions = int(result[-7][off:off+12].strip())
+
+    # Extract excution time as reported by MiniSat.
+    time = float(result[-3][off:-2])
+
+    # Extract whether the formula is satisfiable or not.
+    satisfiability = ['UNSATISFIABLE', 'SATISFIABLE'].index(result[-1])
+
+    return (satisfiability, time, decisions)
 
 def solve(n, k, l):
-    cnf = randomize(n, k, l)
-
-    dimacs = '\n'.join(
-        ['c Team White'] +
-        ['p cnf {} {}'.format(n, l)] +
-        [' '.join(map(str, clause + [0])) for clause in cnf]
-    )
-
     ifd, ifname = mkstemp()
 
     # Write CNF to the input file in DIMACS format.
-    with open(ifd, 'w') as f: f.write(dimacs)
+    with open(ifd, 'w') as f: f.write('\n'.join(
+        ['c Team White'] +
+        ['p cnf {} {}'.format(n, l)] +
+        [' '.join(map(str, clause + [0])) for clause in randomize(n, k, l)]
+    ))
+    
     proc = run(['minisat', '-verb=2', ifname], stderr=STDOUT, stdout=PIPE)
+
+    # Clean up once we are done. Otherwise we *will* run out of space.
+    remove(ifname)
+
     return decode(proc.stdout.decode('utf-8'))
 
-def p(n, k, l):
-    x = 0.0
-    totaltime = 0.0
-    for i in range(int(m)):
-        (satisfiability, time) = solve(n, k, l)
-        if satisfiability:
-            x += 1.0
-        totaltime += time
-
-    return (x / m, totaltime / m)
-
 def main():
-    with open('data.dat', 'w+') as f:
-        f.write('# r {}\n'.format(N))
-
+    with open('data-huge.dat', 'w+') as f:
         r = R[0]
         while r <= R[1]:
-            # r = l / n
-            # l = r * n
-            f.write('{:4.3f}\t'.format(r) + '\t'.join(['{:4.3f} {:4.3f}'.format(*p(n, k, int(r * n))) for n in N]) + '\n')
-            r += R[2] + 0.07 * (r - R[0] - ((R[1] - R[0]) / 2.0)) ** 2.0
+            f.write(dots(r))
+            # For static delta:
+            r += R[2]
+            # For dynamic delta with "focal point" in the middle of the range:
+            #r += R[2] + 0.07 * (r - R[0] - ((R[1] - R[0]) / 2.0)) ** 2.0
 
 if __name__ == '__main__':
     main()
