682b1a998e471d8dda12d41c15b47a5a86a5bf3d
uomoinverde <filippo@openmailbox.org>
2018-03-08 16:46:02 +0100

    Adds documentation.

--- Labs/2018-03-07/data.py
+++ Labs/2018-03-07/data.py
@@ -26,14 +26,36 @@ m = 200.0
 R = (2.0, 6.0, 0.01)
 
 def randomize(n, k, l):
+    """A k-CNF formulae generator.
+
+    :param n: number of variables in the formula
+    :param k: number of literals per clause
+    :param l: number of clauses in the formula
+    :returns: a list of length l, where each element is a list of k different non-zero elements in the range [-n..n]
+    """
     return [[p * -1 if choice([True, False]) else p for p in sample(range(1, n+1), k)] for j in range(l)]
 
 def average(n, k, l):
+    """Routine that returns the mean value of the results obtained after running m experiments.
+
+    :param n: number of variables in the formula
+    :param k: number of literals per clause
+    :param l: number of clauses in the formula
+    :returns: a tuple, containing the average satisfiability ('1' for SATISFIABLE/'0' for UNSATISFIABLE),
+              the average CPU time for an experiment and the average number of decisions taken by the MiniSat solver
+    """
     # r = l / n
     # l = r * n
     return tuple([sum(x) / m for x in zip(*[solve(n, k, l) for i in range(int(m))])])
 
 def dots(r):
+    """Routine that provides, for each chosen value in [R[0]..R[1]], the mean values of the samples obtained
+    by changing the value of literals per clause and variables considered and running a fixed number of experiments.
+
+    :param r: a floating-point number in the range [R[0]..R[1]]
+    :returns: a string, representing a table of records; each record is of the form 'J K L', where
+              (J,K,L) = average(n, k, int(r * n)) for some n (number of variables) and k (literals per clause)
+    """
     return '{:4.3f}'.format(r) + '\t' + '\t'.join([
         '\t'.join(
             [
@@ -43,6 +65,12 @@ def dots(r):
     ]) + '\n'
 
 def decode(result):
+    """Routine that returns relevant information about a run of MiniSat.
+
+    :param result: output of the an execution of MiniSat
+    :returns: a triple containing the response of MiniSat ('1' for SATISFIABLE/'0' for UNSATISFIABLE), the CPU time and
+              the number of decisions taken by MiniSat during its execution
+    """
     result = result.strip().split('\n')
 
     # MiniSat output is in a two-column style, this is the offset
@@ -61,6 +89,14 @@ def decode(result):
     return (satisfiability, time, decisions)
 
 def solve(n, k, l):
+    """Routine that solves a k-SAT instance with n variables and l clauses.
+
+    :param n: number of variables in the formula
+    :param k: number of literals per clause
+    :param l: number of clauses in the formula
+    :returns: a triple containing the response of MiniSat ('1' for SATISFIABLE/'0' for UNSATISFIABLE), the CPU time and
+              the number of decisions taken by MiniSat during its execution
+    """
     ifd, ifname = mkstemp()
 
     # Write CNF to the input file in DIMACS format.
@@ -77,6 +113,7 @@ def solve(n, k, l):
 
     return decode(proc.stdout.decode('utf-8'))
 
+# Entry point of the program.
 def main():
     with open('data-huge.dat', 'w+') as f:
         r = R[0]
