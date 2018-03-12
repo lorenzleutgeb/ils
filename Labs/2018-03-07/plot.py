f51c5cbcb03d993f0c2f09dc730e6bd848735645
Lorenz Leutgeb <lorenz.leutgeb@stud-inf.unibz.it>
2018-03-12 18:16:30 +0100

    Improve plots

--- Labs/2018-03-07/plot.py
+++ Labs/2018-03-07/plot.py
@@ -1,9 +1,5 @@
-
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import pandas.tseries as ts
-from matplotlib.ticker import NullFormatter
+import numpy             as np
 
 setups = [
     (2, [20, 50, 100, 200, 500]),
@@ -11,68 +7,66 @@ setups = [
     (5, [50])
 ]
 
-colors = ['orange', 'dimgray','red', 'green',  'slategrey']
-lighter = ['khaki', 'grey','salmon', 'yellowgreen',  'lightgrey']
-decisionColors =['darkgoldenrod', 'black', 'darkred', 'darkolivegreen', 'lightslategray']
-
+colors = [
+    ('red', 'tomato'),
+    ('xkcd:blue', 'xkcd:azure'),
+    ('orange', 'khaki'),
+    ('green', 'yellowgreen'),
+    ('purple', 'violet'),
+]
 
 for setup in setups:
     k, N = setup
     data = np.loadtxt('data-{}.dat'.format(k))
+
+    xs = data[:,0]
+
+    # Satisfiability plot:
     f = plt.figure()
 
-#    df = {'x': data[:,0]}
-#    for i, n in enumerate(N):
-#        df['n={}'.format(n)] = data[:,1 + i * 3]
-
-#    df = pd.DataFrame(df)
-
-#    for i, n in enumerate(N):
-#        plt.plot('x', 'n={}'.format(n), data=df, marker='o', markerfacecolor=colors[i], markersize=2, color=lighter[i], linewidth=1)
-#        plt.axhline(0.5, color='skyblue', linestyle='dashed', linewidth=1)
-
-#time plots
-    f1 = plt.figure()
-    ax1 = f1.add_subplot(111)
-    dftime = {'x': data[:,0]}
-    for i, n in enumerate(N):
-      dftime['n={}'.format(n)] = data[:,2 + i * 3]
-
-    dftime = pd.DataFrame(dftime)
+    # Mark crossover.
+    plt.axhline(0.5, color='black', linestyle='--', linewidth=1)
 
     for i, n in enumerate(N):
-        ax1.plot('x', 'n={}'.format(n), data=dftime, marker='o', markerfacecolor=colors[i], markersize=2, color=lighter[i], linewidth=1)
-        ax1.set_ylabel('time')
-        plt.yscale('log')
-        plt.ylim(ymax = 1)
-        plt.ylim(ymin = -10)
-        plt.legend()
+        plt.plot(xs, data[:,1 + i * 3], '-o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2)
 
-#decision plots
-    f2 = plt.figure()
-    ax2 = ax1.twinx()
-    dfD = {'x': data[:,0]}
-    for i, n in enumerate(N):
-       dfD['n={}'.format(n)] = data[:,3 + i * 3]
-
-    dfD = pd.DataFrame(dfD)
-
-    for i, n in enumerate(N):
-        ax2.plot('x', 'n={}'.format(n), data=dfD, marker='o', markerfacecolor=decisionColors[i], markersize=5, color=lighter[i], linewidth=5)
-        ax2.set_ylabel('decision')
-        plt.yscale('log')
-
-    if k == 5:
-        plt.ylim(ymax = 1000000)
-        plt.ylim(ymin = 0)
-    else:
-        plt.yscale('log')
-        plt.ylim(ymax = 100000)
-        plt.ylim(ymin = 0)
-
-    #plt.legend()
-    #plt.show()
+    plt.legend(N, title='$n$')
     f.savefig('satisfiablity-{}.pdf'.format(k))
-    #f1.savefig('time-{}.pdf'.format(k))
-    #f2.savefig('decision-{}.pdf'.format(k))
-    f1.savefig('combined-{}.pdf'.format(k))
+
+    # Combined plots (decisions/time):
+    f = plt.figure()
+    ax1 = plt.axes()
+
+    plt.title('Hardness\n($k = {}$, 100 experiments per dot)'.format(k))
+    ax1.set_xlabel('$r = \\frac{l}{n}$')
+    ax1.set_autoscaley_on(True)
+
+    # By default, we have no second y-axis.
+    ax2 = None
+
+    if k in {3, 5}:
+        # Switch to logarithmic scaling and set limits.
+        ax1.set_yscale('log')
+
+        # Add second y-axis for execution time.
+        ax2 = ax1.twinx()
+        ax2.set_yscale('log')
+        ax2.set_autoscaley_on(True)
+
+    for i, n in enumerate(N):
+        ax1.plot(xs, data[:,3 + i * 3], '-o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2)
+        ax1.set_ylabel('Decisions [1]')
+
+    for i, n in enumerate(N):
+        if k == 2:
+            continue
+
+        ax2.plot(xs, data[:,2 + i * 3], '--o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2)
+        ax2.set_ylabel('Average Execution Time [s]')
+
+    if ax2 != None:
+        plt.gca().add_artist(ax2.legend(N, title='$n$'))
+
+    ax1.legend(N, loc=(3 if k == 2 else 2), title='$n$')
+
+    f.savefig('combined-{}.pdf'.format(k))
