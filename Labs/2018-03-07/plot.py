65c9e6bcda75d7fffa34a8feaa198a1f66bb82ea
Lorenz Leutgeb <lorenz.leutgeb@stud-inf.unibz.it>
2018-03-12 20:11:47 +0100

    Improve plots

--- Labs/2018-03-07/plot.py
+++ Labs/2018-03-07/plot.py
@@ -23,26 +23,27 @@ for setup in setups:
 
     # Satisfiability plot:
     f = plt.figure()
+    plt.title('Satisfiability\n($k = {}$, 100 experiments per dot)'.format(k))
+    plt.axes().set_xlabel('$r = \\frac{l}{n}$')
+
+    [plt.plot(
+        xs, data[:,1 + i * 3], '-o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2
+    ) for i in range(len(N))]
+
+    plt.legend(N, title='$n$')
 
     # Mark crossover.
     plt.axhline(0.5, color='black', linestyle='--', linewidth=1)
 
-    for i, n in enumerate(N):
-        plt.plot(xs, data[:,1 + i * 3], '-o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2)
-
-    plt.legend(N, title='$n$')
     f.savefig('satisfiability-{}.pdf'.format(k))
 
     # Combined plots (decisions/time):
     f = plt.figure()
-    ax1 = plt.axes()
-
     plt.title('Hardness\n($k = {}$, 100 experiments per dot)'.format(k))
+    ax1 = plt.axes()
     ax1.set_xlabel('$r = \\frac{l}{n}$')
     ax1.set_autoscaley_on(True)
-
-    # By default, we have no second y-axis.
-    ax2 = None
+    ax1.set_ylabel('Decisions [1]')
 
     if k in {3, 5}:
         # Switch to logarithmic scaling and set limits.
@@ -50,22 +51,19 @@ for setup in setups:
 
         # Add second y-axis for execution time.
         ax2 = ax1.twinx()
+        ax2.set_ylabel('Average Execution Time [s]')
         ax2.set_yscale('log')
         ax2.set_autoscaley_on(True)
 
-    for i, n in enumerate(N):
-        ax1.plot(xs, data[:,3 + i * 3], '-o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2)
-        ax1.set_ylabel('Decisions [1]')
+        [ax2.plot(
+            xs, data[:,2 + i * 3], '--o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2
+        ) for i in range(len(N))]
 
-    for i, n in enumerate(N):
-        if k == 2:
-            continue
-
-        ax2.plot(xs, data[:,2 + i * 3], '--o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2)
-        ax2.set_ylabel('Average Execution Time [s]')
-
-    if ax2 != None:
         plt.gca().add_artist(ax2.legend(N, title='$n$'))
+
+    [ax1.plot(
+        xs, data[:,3 + i * 3], '-o', markerfacecolor=colors[i][1], markersize=3, color=colors[i][0], linewidth=2
+    ) for i in range(len(N))]
 
     ax1.legend(N, loc=(3 if k == 2 else 2), title='$n$')
 
