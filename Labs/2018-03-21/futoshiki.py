6d23855c6e07a61a8e518dd09e0dc444daf5c19b
Lorenz Leutgeb <lorenz.leutgeb@stud-inf.unibz.it>
2018-03-21 23:15:15 +0100

    Fix futoshiki.py

--- Labs/2018-03-21/futoshiki.py
+++ Labs/2018-03-21/futoshiki.py
@@ -1,10 +1,11 @@
-import pyeda
-from pyeda.inter import *
+# Team White
 
-from sys import stdin
+from pyeda.inter import exprvars, And, Implies, Not, OneHot, Or
+from itertools   import combinations, product
+from sys         import stdin
 
 def parse(pair):
-    return tuple(map(int, pair[1:-1].split(',')))
+    return tuple(map(lambda x: int(x) - 1, pair[1:-1].split(',')))
 
 def read(stream):
     gt = []
@@ -23,85 +24,64 @@ def read(stream):
         if line == '':
             continue
 
-        tk = line.split(' ')
+        a, b = line.split(' ')
 
-        if tk[0] == '>':
-            r, c = parse(tk[1])
-            gt.append(((r, c), (r, c+1)))
-        elif tk[0] == '<':
-            r, c = parse(tk[1])
-            gt.append(((r, c), (r, c-1)))
-        elif tk[0] == '^':
-            r, c = parse(tk[1])
-            gt.append(((r-1, c), (r, c)))
-        elif tk[0] == 'v':
-            r, c = parse(tk[1])
-            gt.append(((r+1, c), (r, c)))
-        elif tk[0] == 'size':
-            n = int(tk[1])
+        if a == 'size':
+            n = int(b)
+            continue
+
+        r, c = parse(b)
+
+        if a == '>':
+            gt.append(((r, c), (r, c + 1)))
+        elif a == '<':
+            gt.append(((r, c), (r, c - 1)))
+        elif a == '^':
+            gt.append(((r, c), (r - 1, c)))
+        elif a == 'v':
+            gt.append(((r, c), (r + 1, c)))
         else:
-            r, c = parse(tk[1])
-            fix.append((r, c, int(tk[0])))
+            fix.append((r, c, int(a) - 1))
 
-    return (n, gt, fix)
+    return n, gt, fix
 
-def encode(n, gt, fix):
-    futo = exprvars('x', (0,n), (0,n), (0,n))
-    # Each cell on the board contains a unique value.
-    vals = And(*[
-             And(*[
-                 OneHot(*[futo[row,col,val]
-                     for val in range(0,n)])
-                 for col in range(0,n)])
-             for row in range(0,n)])
-    # For each row, all cells have distinct values.
-    rows = And(*[
-             And(*[
-                 OneHot(*[futo[row,col,val]
-                     for col in range(0,n)])
-                 for val in range(0,n)])
-             for row in range(0,n)])
-    # For each column, all cells have distinct values.
-    cols = And(*[
-             And(*[
-                 OneHot(*[futo[row,col,val]
-                     for row in range(0,n)])
-                 for val in range(0,n)])
-             for col in range(0,n)])
-    
-    ineqs = And(*[
-              Or(*[
-                Or(*[
-                    And(futo[r1,c1,i],futo[r2,c2,j])
-                    for i in range(j+1,n)])
-                for j in range(0,n-1)])
-              for ((r1,c1),(r2,c2)) in gt])
-    fixes = And(*[futo(row,col,val) for (row,col,val) in fix])
+def solve(n, gt, fix):
+    r = (0, n)
+    d, f = range(*r), exprvars('f', r, r, r)
+    return f, And(
+        *[  # Each cell contains a unique value.
+            OneHot(*[f[r, c, v] for v in d])
+            for r, c in product(d, d)
+        ],
+        *[  # For each row, all cells have distinct values.
+            OneHot(*[f[r, c, v] for c in d])
+            for r, v in product(d, d)
+        ],
+        *[  # Inequalities
+            Implies(f[r1,c1,v1], Not(f[r2,c2,v2]))
+            for ((r1,c1), (r2,c2)), (v1, v2) in product(gt, combinations(d, 2))
+        ],
+        *[  # Initial assignment
+            f[r, c, v] for r, c, v in fix
+        ],
+        *[  # For each column, all cells have distinct values.
+            OneHot(*[f[r, c, v] for r in d])
+            for c, v in product(d, d)
+        ]
+    ).satisfy_one()
 
-    # Conjunction of constraints.
-    sol = And(vals,rows,ineqs,fixes,cols)
+def display(f, model):
+    if model == None:
+        print('UNSOLVABLE')
+        return
 
-    return (sol, futo)
+    d = range(0, len(f))
+    print('\n'.join([
+        ' '.join([
+            ''.join([
+                str(v + 1) if model[f[r, c, v]] else '' for v in d
+            ]) for c in d
+        ]) for r in d
+    ]))
 
-def solve(grid, futo):
-    return (grid.satisfy_one(), futo)
-
-def decode(point, futo, row, col, n):
-    digits=range(0,n)
-    for val in digits:
-        if point[futo[row,col,val]]:
-            return str(digits[val-1]+1)
-    return "X"
-
-def display(point, futo,n):
-    digits=range(0,n)
-    chars = list()
-    for row in digits:
-        for col in digits:
-            chars.append(decode(point, futo, row, col, n))
-        chars.append('\n')
-
-    print("".join(chars))
-    
-n, gt, fix  = read(stdin)
-display(*solve(*encode(n, gt, fix)), n)
+display(*solve(*read(stdin)))
