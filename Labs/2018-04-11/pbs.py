25de67f5158fd26414c486bc058d1c692ba67a15
uomoinverde <filippo@openmailbox.org>
2018-04-11 17:56:26 +0200

    parse complete.

--- Labs/2018-04-11/pbs.py
+++ Labs/2018-04-11/pbs.py
@@ -1,41 +1,77 @@
-from sys import exit
+from sys import argv, exit
 
-def parse(f):
-  for lno, raw in enumerate(f):
-    line = raw.split(' ')
+def sgn_change(lst):
+  return list(map(lambda x: -x, lst))
 
-    if line[0] == "*":
-      continue
-
-    # Ignoring optimization constraints for now.
-    if line[0] == "min:":
-      continue
-
-    ops = ["=", "<=", "<", ">=", ">"]
-
-    coeff = [line[2*i] for i in range(len(line)//2 - 1)]
-    vars = [line[2*i+1] for i in range(len(line)//2 - 1)]
-
-    op, term = line[:-2], line[:-1]
-
-    if not (op in ops):
-      print("input malformed at line {}: incorrect symbol for (in)equality.".format(lno))
+def get_varkey(lno, var):
+  if (var[0] == "x"):
+    try:
+      key = int(var[1:])
+    except ValueError:
+      print("input malformed at line {}: {} is not a correct variable.".format(lno,var))
       exit(1)
-    
-    # for (a, x) in zip(coeff,var):
-    #  if not ()
+    return key
+  else:
+    print("input malformed at line {}: {} is not a correct variable.".format(lno,var))
+    exit(1)
 
-    # for (a,b) in zip(coeff,var):
-    #   if a in ops:
-    #     if a == ">=" or a == "=":
-    #       continue
-    #     if a == ">":
-    #       fst[:-1] = ">="
-    #       snd[:-1] -= 1
-    #     if a == "<=":
-    #       for i in range(len(fst)-1):
-    #         fst[i] *= -1
-    #     if a == "<":
-    #       fst = 
+def parse(filename):
+  """
+  :returns: (dict, c) representing sum(l) >= c.
+  """
+  c = 0
+  coeffs = {}
+
+  with open(filename, 'r') as f:
+    for lno, raw in enumerate(f):
+      line = raw.strip('\n').strip(';').split(' ')
+
+      if line[0] == "*":
+        continue
+
+      # Ignoring optimization constraints for now.
+      if line[0] == "min:":
+        continue
+
+      ops = ["=", "<=", "<", ">=", ">"]
+
+      cofs = [line[2*i] for i in range(len(line)//2 - 1)]
+      vars = [line[2*i+1] for i in range(len(line)//2 - 1)]
+
+      term, op = line[-1], line[-2]
+
+      try:
+        term = int(term)
+      except ValueError:
+        print("input malformed at line {}: {} is not an integer coefficient.".format(lno, term))
+        exit(1)
+
+      if not (op in ops):
+        print("input malformed at line {}: incorrect symbol for (in)equality.".format(lno))
+        exit(1)
+
+      if (op == ">"):
+        term += 1
+      elif op == "<=":
+        cofs = sgn_change(cofs)
+      elif op == "<":
+        cofs = sgn_change(cofs)
+        term -= 1
+
+      for (a, x) in zip(cofs,vars):
+        try:
+          t = int(a)
+        except ValueError:
+          print("input malformed at line {}: {} is not an integer coefficient.".format(lno, a))
+          exit(1)
+
+        key = get_varkey(lno, x[(1 if x[0] == "~" else 0):])
+        coeffs.update([(key, coeffs.get(key,0) + (-t if x[0] == "~" else t))])
+
+        term = term - (t if x[0] == "~" else 0)
+      
+      c += term
+
+  return(coeffs, c)
 
 
