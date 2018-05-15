a66aecda8f3bcc4b06c407591c99c90f737098bc
Aneta.Koleva <aneta.koleva@stud-inf.unibz.it>
2018-05-15 19:24:23 +0200

    Make facts for the world

--- Projects/wumpus/PyAgent.py
+++ Projects/wumpus/PyAgent.py
@@ -1,5 +1,5 @@
 # PyAgent.py
-
+from tempfile   import mkstemp
 import Action
 import Orientation
 
@@ -7,9 +7,9 @@ N = 4
 
 # Initially we do not know about stench/breeze/glitter
 # anywhere.
-world = [[(None, None, None)] * N] * N
+world = [[(None, None, None) for i in range(N)] for j in range(N)]
 
-position = (1, 1)
+position = (0, 0)
 
 orientation = Orientation.RIGHT
 
@@ -28,6 +28,27 @@ def PyAgent_Process (stench,breeze,glitter,bump,scream):
         print "We bumped. Should never happen."
 
     world[position[0]][position[1]] = (stench == 1, breeze == 1, glitter == 1)
+
+    perception = [
+        "position(" + str(position[0]) + "," + str(position[1]) + ").",
+        "orientation(" + str(orientation) + ")."
+    ]
+
+    for i, row in enumerate(world):
+        for j, (stench, breeze, glitter) in enumerate(row):
+            if stench is not None:
+                prefix = "-" if not stench else ""
+                perception.append(prefix + "stench({},{}).".format(i,j))
+            if breeze is not None:
+                prefix = "-" if not breeze else ""
+                perception.append(prefix + "breeze({},{}).".format(i,j))
+            if glitter is not None:
+                prefix = "-" if not stench else ""
+                perception.append(prefix + "glitter({},{}).".format(i,j))
+
+    # make a temp file and add perception there then pass that to dlv
+
+    print("\n".join(perception))
 
     # 1. Add the knowledge that we obtain as percepts to
     #    our model/state of the world. We should
@@ -61,3 +82,7 @@ def PyAgent_Process (stench,breeze,glitter,bump,scream):
 
 def PyAgent_GameOver (score):
     print "PyAgent_GameOver: score = " + str(score)
+
+
+if __name__ == '__main__':
+    PyAgent_Process(0, 1, 0, 0, 0)
