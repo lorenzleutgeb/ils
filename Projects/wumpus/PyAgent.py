7b1356034b2a0623f010e3e461a7d3e7137a0ca4
Lorenz Leutgeb <lorenz.leutgeb@stud-inf.unibz.it>
2018-05-15 17:30:07 +0200

    wump: Add high-level pseudocode for agent

--- Projects/wumpus/PyAgent.py
+++ Projects/wumpus/PyAgent.py
@@ -10,9 +10,19 @@ def PyAgent_Destructor ():
     print "PyAgent_Destructor"
 
 def PyAgent_Initialize ():
+    # Initialize world!
     print "PyAgent_Initialize"
 
 def PyAgent_Process (stench,breeze,glitter,bump,scream):
+    # 1. Add the knowledge that we obtain as percepts to
+    #    our model/state of the world. We should
+    #    a. not move to dangerous cells (might die).
+    #    b. not shoot the arrow if we aren't sure where
+    #       the Wumpus is.
+    #    c. Once we have the gold, return to where we
+    #       started from (easy: reverse the path).
+    # 2. Run solver.
+    # 3. Return action to the game.
     perceptStr = ""
     if (stench == 1):
         perceptStr += "Stench=True,"
@@ -35,7 +45,7 @@ def PyAgent_Process (stench,breeze,glitter,bump,scream):
     else:
         perceptStr += "Scream=False"
     print "PyAgent_Process: " + perceptStr
-    
+
     return Action.GOFORWARD
 
 def PyAgent_GameOver (score):
